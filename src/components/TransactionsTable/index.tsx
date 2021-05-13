8a3137dae66e9eb5dc48d5491314e00512af7f7f
Lucas Gerhardt de Camargo <lucasgcamargo@icloud.com>
2021-05-13 11:23:04 -0700

    Add api and modal

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+    useEffect(() => {
+        api.get('transactions')
+            .then(response => console.log(response.data))
+    }, []);
+
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Value</th>
+                        <th>Category</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Website Development</td>
+                        <td className="deposit">$12000,00</td>
+                        <td>Development</td>
+                        <td>02/20/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Rental</td>
+                        <td className="withdraw">- $12000,00</td>
+                        <td>House</td>
+                        <td>02/12/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    )
+}
