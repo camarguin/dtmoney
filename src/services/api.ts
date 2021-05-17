0556fae3e0f82319ea38df0b375658af2bf78052
Lucas Gerhardt de Camargo <lucasgcamargo@icloud.com>
2021-05-17 12:40:52 -0700

    Removed gh-pages

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    // baseURL: 'http://localhost:3000/api'
-    baseURL: 'https://camarguin.github.io/dtmoney/api'
+    baseURL: 'http://localhost:3000/api'
 })
