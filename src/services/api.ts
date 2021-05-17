e8187c2f0e5734acd07846cc5b8315bd5480b0a9
Lucas Gerhardt de Camargo <lucasgcamargo@icloud.com>
2021-05-17 15:46:55 -0700

    Add env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    // baseURL: 'http://localhost:3000/api'
-    baseURL: 'https://dtmoneylgc.netlify.app/api'
+    baseURL: `${process.env.REACT_APP_API_URL}/api`
+    // baseURL: 'https://dtmoneylgc.netlify.app/api'
 })
